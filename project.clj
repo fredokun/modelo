4cf4c270ed2349a2c8203c084eef1e06ce17ae69
Fredokun <fredokun@github.com>
2016-05-13 10:17:41 +0200

    lein new

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject specl "0.1.0-SNAPSHOT"
+  :description "When formal methods meet Clojure (and vice-versa)"
+  :url "https://github.com/fredokun/specl"
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [clj-by-example "0.1.0-SNAPSHOT"]])
+
---
+++ test/specl/core_test.clj
@@ -0,0 +1,7 @@
+(ns specl.core-test
+  (:require [clojure.test :refer :all]
+            [specl.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
